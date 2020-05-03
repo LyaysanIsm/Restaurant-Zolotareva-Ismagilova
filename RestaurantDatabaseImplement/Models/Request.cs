5471c02d9832721b7c940a45c686acef7eecea8c
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2020-05-03 22:22:10 +0400

    addmodels

---
+++ RestaurantDatabaseImplement/Models/Request.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantBusinessLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RestaurantDatabaseImplement.Models
+{
+    public class Request
+    {
+        public int? Id { get; set; }
+        public int SupplierId { get; set; }
+        [Required]
+        public DateTime DateCreate { get; set; }
+        public DateTime? DateImplement { get; set; }
+        [Required]
+        public Status Status { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+}
---
+++ RestaurantDatabaseImplement/Models/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RestaurantDatabaseImplement.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public int FridgeId { get; set; }
+        [Required]
+        public string SupplierFIO { get; set; }
+        [Required]
+        public int Password { get; set; }
+    }
+}
