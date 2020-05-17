b9c53b0738ade903d1236ffc502dd3b2d00fc46a
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2020-05-18 00:02:44 +0400

    add helper models

---
+++ RestaurantBusinessLogic/HelperModels/WordInfo.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantBusinessLogic.HelperModels
+{
+    class WordInfo
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public List<DishViewModel> Dishes { get; set; }
+        public List<FridgeViewModel> Fridges { get; set; }
+    }
+}
