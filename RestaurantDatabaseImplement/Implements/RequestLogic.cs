54284386bc50618920fb23abc20ca7e9e6813d73
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2020-05-17 19:07:18 +0400

    corrections

--- RestaurantDatabaseImplement/Implements/RequestLogic.cs
+++ RestaurantDatabaseImplement/Implements/RequestLogic.cs
@@ -4,6 +4,7 @@ using RestaurantBusinessLogic.ViewModels;
 using RestaurantDatabaseImplement.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RestaurantDatabaseImplement.Implements
