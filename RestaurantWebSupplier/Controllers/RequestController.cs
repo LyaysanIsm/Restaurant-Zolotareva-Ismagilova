2fc750e41a0a7fdd88e6eadd6205dd22c7195932
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2020-05-25 17:06:08 +0400

    Work with web

---
+++ RestaurantWebSupplier/Controllers/RequestController.cs
@@ -0,0 +1,44 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantBusinessLogic.BindingModels;
+using RestaurantBusinessLogic.Interfaces;
+using RestaurantWebSupplier.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantWebSupplier.Controllers
+{
+    public class RequestController
+    {
+        private readonly IRequestLogic _requestLogic;
+        private readonly IFoodLogic _foodLogic;
+
+        public RequestController(IRequestLogic requestLogic, IFoodLogic foodLogic)
+        {
+            _requestLogic = requestLogic;
+            _foodLogic = foodLogic;
+        }
+        /*
+        public IActionResult Request()
+        {
+            var requests = _requestLogic.Read(new RequestBindingModel
+            {
+                SupplierId = Program.Supplier.Id
+            });
+            var requestModels = new List<RequestModel>();
+            foreach (var request in requests)
+            {
+                var foods = new List<RequestFoodModel>();
+                foreach (var food in request.Foods)
+                {
+                    var foodata = _foodLogic.Read(new FoodBindingModel
+                    {
+                        Id = food.Key
+                    }).FirstOrDefault();
+                }
+            }
+        }*/
+    }
+}
---
+++ RestaurantWebSupplier/Models/RequestFoodModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantWebSupplier.Models
+{
+    public class RequestFoodModel
+    {
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        public int FoodId { get; set; }
+        public int Count { get; set; }
+    }
+}
